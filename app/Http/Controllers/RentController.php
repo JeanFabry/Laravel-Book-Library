f6d17620a42f584aa098c5532c8f105563a9ddc2
JeanFabry <jeanfabry1435@gmail.com>
2021-02-11 18:15:33 +0100

    updating invoices numbers

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Rent;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Faker\Factory as Faker;
 class RentController extends Controller
 {
     /**
@@ -24,9 +24,20 @@ class RentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request,$id)
     {
-        //
+        $user = $request->user()->id;
+        $faker = Faker::create();
+        $rent = new Rent;
+
+        $rent->user_id = $user;
+        $rent->book_id = $id;
+        $rent->invoice_number=$faker->ean8;
+        $rent->save();
+
+        // Rent::create('book_id'= $bookId, 'user_id'=$user);
+
+        return redirect()->route('main.index');
     }
 
     /**
@@ -35,9 +46,9 @@ class RentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+       
     }
 
     /**
